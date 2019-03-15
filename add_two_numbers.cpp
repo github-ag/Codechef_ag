832c79d0988bdee8aa37f7820faf171c84c752a8
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-03-15 23:01:05 +0530

    Create add_two_numbers.cpp

---
+++ add_two_numbers.cpp
@@ -0,0 +1,14 @@
+•	#include<bits/stdc++.h>
+•	using namespace std;
+•	int main()
+•	{int T,A,B,sum;
+•	scanf("%d",&T);
+•	for(int i=0;i<T;i++)
+•	{scanf("%d%d",&A,&B);
+•	sum=A+B;
+•	 
+•	printf("%d",sum);
+•	cout<<"\n";
+•	}
+•	return 0;
+•	} 
